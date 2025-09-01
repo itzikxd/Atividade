9c3310950e6e413e9532346dca4228a76b6a57d0
itzikxd <isaquesamuel1233107@gmail.com>
2025-09-01 19:52:34 -0300

    Delete Atividade prática directory

--- Atividade prática/script.js
+++
@@ -1,5 +0,0 @@
-function showSendFeedback(evt) {
-// Evita navegação; apenas demonstração visual do formulário
-evt.preventDefault();
-alert("Mensagem registrada visualmente. Para envio real, é preciso um backend.");
-}
