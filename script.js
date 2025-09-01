dc58f221082eec2b146dff22dc248b1e0c2126df
itzikxd <isaquesamuel1233107@gmail.com>
2025-09-01 19:52:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showSendFeedback(evt) {
+// Evita navegação; apenas demonstração visual do formulário
+evt.preventDefault();
+alert("Mensagem registrada visualmente. Para envio real, é preciso um backend.");
+}
